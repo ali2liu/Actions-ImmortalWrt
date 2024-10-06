cdf20d83e6b08c44575104f6a0406ef7532e3be4
ali2liu <ali2liu@gmail.com>
2024-10-06 18:14:06 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,12 +3,6 @@
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-cd ./immortalwrt/package/feeds/
-rm -rf luci/luci-app-homeproxy
-cd ..
-cd ..
-cd ./immortalwrt/feeds/luci/applications/
-rm -rf luci-app-homeproxy
-cd ..
-cd ..
-cd ..
+rm -rf package/feeds/luci/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/muink/homeproxy.git package/homeproxy
