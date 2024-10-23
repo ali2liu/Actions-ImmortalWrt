f65c9ee1fa98392f5d873de1d5c6940802e3f2a2
ali2liu <ali2liu@gmail.com>
2024-10-23 08:07:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -3,6 +3,6 @@
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-#rm -rf package/feeds/luci/luci-app-homeproxy
-#rm -rf feeds/luci/applications/luci-app-homeproxy
-#git clone https://github.com/muink/homeproxy.git package/homeproxy
+rm -rf package/feeds/luci/luci-app-homeproxy
+rm -rf feeds/luci/applications/luci-app-homeproxy
+git clone https://github.com/muink/homeproxy.git package/homeproxy
